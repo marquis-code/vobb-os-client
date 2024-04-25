8f567d13655ec1523d49facb8d65cd30a807c98e
Favour Adekogbe <favouradekogbe@gmail.com>
2024-04-25 07:49:15 +0100

    updated ui and fixed yarn lock

--- src/modules/authentication/emailVerification/verifyEmail/index.tsx
+++ src/modules/authentication/emailVerification/verifyEmail/index.tsx
@@ -17,7 +17,7 @@ const VerifyEmailUI: React.FC<VerifyEmailProps> = ({ submit }) => {
       <section className="bg-circle-pattern max-w-[400px] m-auto text-vobb_neutral-100 bg-no-repeat bg-[length:600px_600px] bg-[center_top_-100px] pt-[100px] px-4 pb-4">
         <EnvelopeClosedIcon className="mb-12 mt-2 mx-auto w-8 h-8" />
 
-        <h1 className="text-xl sm:text-2xl font-bold mb-8 text-vobb-neutral-100 text-center">
+        <h1 className="text-xl sm:text-2xl font-bold mb-4 text-vobb-neutral-100 text-center">
           Check your email
         </h1>
         <p className="text-center mb-8">We sent a verification code to anjola@vobb.io</p>
