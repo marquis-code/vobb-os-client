797b725530798ce75a9b22d114216b67da09c359
Adaeze <hamsaikemefuna@gmail.com>
2024-05-09 10:04:00 +0100

    updated reset code for forgot password

--- src/modules/authentication/forgotPassword/verifyPassword/index.tsx
+++ src/modules/authentication/forgotPassword/verifyPassword/index.tsx
@@ -1,27 +1,42 @@
 import { Button, CustomInputOTP } from "components";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ArrowLeftIcon, EnvelopeClosedIcon } from "@radix-ui/react-icons";
 import { Routes } from "router";
 import { forgotPasswordParams } from "types";
 
 interface VerifyPasswordProps {
   submit: (data: forgotPasswordParams) => void;
+  handleResend: () => void;
+  loading: boolean;
 }
 
-const VerifyPasswordUI: React.FC<VerifyPasswordProps> = ({ submit }) => {
+const VerifyPasswordUI: React.FC<VerifyPasswordProps> = ({ submit, handleResend, loading }) => {
   const navigate = useNavigate();
   const [otp, setOtp] = useState("");
+  const [countdown, setCountdown] = useState(30);
   const [searchParams] = useSearchParams();
   const encodedToken = searchParams.get("token");
   const encodedEmail = searchParams.get("email");
   const token = encodedToken ? decodeURIComponent(encodedToken) : null;
   const email = encodedEmail ? decodeURIComponent(encodedEmail) : null;
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     submit({ token, otp });
   };
+  const submitResend = () => {
+    handleResend();
+    setCountdown(30);
+  };
 
+  useEffect(() => {
+    let timer;
+    if (countdown > 0) {
+      timer = setTimeout(() => setCountdown(countdown - 1), 1000);
+    }
+    return () => clearTimeout(timer);
+  }, [countdown]);
   return (
     <main>
       <section className="bg-circle-pattern max-w-[400px] m-auto text-vobb_neutral-100 bg-no-repeat bg-[length:600px_600px] bg-[center_top_-100px] pt-[100px] px-4 pb-4">
@@ -43,8 +58,12 @@ const VerifyPasswordUI: React.FC<VerifyPasswordProps> = ({ submit }) => {
         </form>
         <p className="cursor-pointer mx-auto mt-6 text-center">
           Didn't receive the code?
-          <Button className="text-vobb-primary-70" variant={"ghost"}>
-            Click to resend
+          <Button
+            className="text-vobb-primary-70"
+            onClick={submitResend}
+            disabled={countdown > 0}
+            variant={"ghost"}>
+            {countdown > 0 ? countdown : "Click to resend "}
           </Button>
         </p>
         <Button
